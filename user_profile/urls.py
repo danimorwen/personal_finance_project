e03b2b656712d0f519adb6b007b8ca77e5f31766
Danielle Pinho <danidpinho@gmail.com>
2023-07-04 11:56:09 +0100

    add management endpoint

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("home/", views.home, name="home")]
+urlpatterns = [
+    path("home/", views.home, name="home"),
+    path("management/", views.management, name="management"),
+]
--- user_profile/views.py
+++ user_profile/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse(render(request, "home.html"))
+
+
+def management(request):
+    return HttpResponse(render(request, "management.html"))
